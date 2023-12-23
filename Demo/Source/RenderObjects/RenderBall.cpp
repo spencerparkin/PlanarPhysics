2fb4e7de95a0642e597f7a657d444661f770a28e
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2023-12-22 17:56:43 -0700

    One more to-do item.

--- Demo/Source/RenderObjects/RenderBall.cpp
+++ Demo/Source/RenderObjects/RenderBall.cpp
@@ -16,6 +16,7 @@ RenderBall::RenderBall()
 
 /*virtual*/ void RenderBall::Render(DrawHelper& drawHelper) const
 {
+	// TODO: Draw spokes when we add angular velocity and orientation to the ball.
 	drawHelper.DrawCircle(this->position, this->radius, this->r, this->g, this->b, 32);
 }
 
